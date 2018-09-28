b057fdcd8a2b1b834ed5544c10cbd72f8399242a
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2018-09-28 15:20:52 -0700

    adding the name, email and photos of the robos dynamically

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hi Man',
+        username: 'Bret',
+        email: 'hey@april.biz'
+    },
+    {
+        id: 2,
+        name: 'SuperMan',
+        username: 'Antonette',
+        email: 'sup@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Xman',
+        username: 'Samantha',
+        email: 'xman@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'ShaktiMan',
+        username: 'Karianne',
+        email: 'shakti.man@kory.org'
+    },
+    {
+        id: 5,
+        name: 'BatMan',
+        username: 'Kamren',
+        email: 'Lucio@annie.ca'
+    },
+    {
+        id: 6,
+        name: 'HolaMan',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz'
+    }
+];
